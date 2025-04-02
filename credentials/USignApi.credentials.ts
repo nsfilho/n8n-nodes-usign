978e6c4b022484d9ec6880c0fef5e1f09ae3532f
Nelio <nsfilho@icloud.com>
2025-04-02 18:03:12 -0300

    feat: initial version

---
+++ credentials/USignApi.credentials.ts
@@ -0,0 +1,62 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class USignApi implements ICredentialType {
+	name = 'uSignApi';
+	displayName = 'uSign Credentials API';
+	properties: INodeProperties[] = [
+		// The credentials to get from user and save encrypted.
+		// Properties can be defined exactly in the same way
+		// as node properties.
+		{
+			displayName: 'Username',
+			name: 'username',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Password',
+			name: 'password',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+		},
+		{
+			displayName: 'URL Base',
+			name: 'urlBase',
+			type: 'string',
+			default: 'https://api.fusion.usign.io',
+		},
+	];
+
+	// This credential is currently not used by any node directly
+	// but the HTTP Request node can use it to make requests.
+	// The credential is also testable due to the `test` property below
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			auth: {
+				username: '={{ $credentials.username }}',
+				password: '={{ $credentials.password }}',
+			},
+			qs: {
+				// Send this as part of the query string
+				n8n: 'rocks',
+			},
+		},
+	};
+
+	// The block below tells how this credential can be tested
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{ $credentials.urlBase }}',
+			url: '',
+		},
+	};
+}
